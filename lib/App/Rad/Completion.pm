17df6a1c1889981e44bb9c9516ae79d894f43da8
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2012-06-09 00:58:25 -0300

    Starting POD

--- lib/App/Rad/Completion.pm
+++ lib/App/Rad/Completion.pm
@@ -111,3 +111,93 @@ sub do_completion {
 }
 
 42
+
+__END__
+
+=head1 NAME
+
+App::Rad::Completion - The great new App::Rad::Completion!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+
+=head1 SYNOPSIS
+
+    app-rad-completion init ~/.bashrc
+    . ~/.bashrc
+
+=head1 COMMANDS
+
+=head2 add_program
+
+=head2 complete
+
+=head2 generate
+
+=head2 help
+
+=head2 init
+
+=head2 remove_program
+
+=cut
+
+=head1 AUTHOR
+
+Fernando Correa de Oliveira, C<< <fco at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-app-rad-completion at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Rad-Completion>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc App::Rad::Completion
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-Rad-Completion>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/App-Rad-Completion>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/App-Rad-Completion>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/App-Rad-Completion/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Fernando Correa de Oliveira.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
